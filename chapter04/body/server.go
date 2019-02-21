e70dbbb083025520088ad95a9124af458dd665ef
EvanXzj <2686465170@qq.com>
2019-02-21 15:46:21 +0800

    listing 4.3 done

---
+++ chapter04/body/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func body(w http.ResponseWriter, r *http.Request) {
+	length := r.ContentLength
+	body := make([]byte, length)
+	r.Body.Read(body)
+	fmt.Fprintln(w, string(body))
+}
+
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:3000",
+	}
+	http.HandleFunc("/body", body)
+	server.ListenAndServe()
+}
