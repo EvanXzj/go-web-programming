f88f64ceb477c661fc1ea32e787dabaaf104a6dd
EvanXzj <2686465170@qq.com>
2019-02-28 18:20:59 +0800

    listing 8.11 done

---
+++ chapter08/httptest_2/server_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"os"
+)
+
+var mux *http.ServeMux
+var recorder *httptest.ResponseRecorder
+
+func TestMain(m *testing.M) {
+	setUp()
+	code := m.Run()
+	tearDown()
+	os.Exit(code)
+}
+
+func setUp() {
+	mux = http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+	recorder = httptest.NewRecorder()
+
+}
+
+func TestHandleGet(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(recorder, request)
+
+	if recorder.Code != 200 {
+		t.Errorf("Response code is %v", recorder.Code)
+	}
+	var post Post
+	json.Unmarshal(recorder.Body.Bytes(), &post)
+	if post.Id != 1 {
+		t.Errorf("Cannot retrieve JSON post")
+	}
+}
+
+func TestHandlePut(t *testing.T) {
+	json := strings.NewReader(`{"content":"Updated Post","author":"Sau Sheong"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	mux.ServeHTTP(recorder, request)
+
+	if recorder.Code != 200 {
+		t.Errorf("Response code is %v", recorder.Code)
+	}
+}
+
+func tearDown() {
+
+}
