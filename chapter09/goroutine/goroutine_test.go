3cba7d2a492f2f7c3bfcce817a6b20d78a54e72c
EvanXzj <2686465170@qq.com>
2019-03-01 15:11:14 +0800

    listing 9.1 done

---
+++ chapter09/goroutine/goroutine_test.go
@@ -0,0 +1,44 @@
+package main
+
+import "testing"
+// import "time"
+
+// func TestPrint1(t *testing.T) {
+//   print1()
+// }
+
+// func TestGoPrint1(t *testing.T) {
+//   goPrint1()
+//   time.Sleep(1 * time.Millisecond)
+// }
+
+// func TestGoPrint2(t *testing.T) {
+// 	goPrint2()
+// 	time.Sleep(1 * time.Millisecond)
+// }
+
+// // Benchmark cases
+// go test -v -run x -bench . -cpu 4
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
+}
+
+func BenchmarkGoPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
+}
+
+func BenchmarkPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print2()
+	}
+}
+
+func BenchmarkGoPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint2()
+	}
+}
